457614c63510c3d5eacc67e33a8012586cea259d
Alexander Alekhin <alexander.alekhin@itseez.com>
2014-07-01 20:02:02 +0400

    ocl: fix mac and superres test

--- modules/ocl/include/opencv2/ocl/cl_runtime/cl_runtime.hpp
+++ modules/ocl/include/opencv2/ocl/cl_runtime/cl_runtime.hpp
@@ -5,9 +5,18 @@
 
 #if defined(HAVE_OPENCL_STATIC)
 
-#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
-
 #if defined __APPLE__
+// APPLE ignores CL_USE_DEPRECATED_OPENCL_1_1_APIS so use this hack:
+#include <OpenCL/cl_platform.h>
+#ifdef CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+#undef CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+#define CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+#endif
+#ifdef CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+#undef CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+#define CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+#endif
+
 #include <OpenCL/cl.h>
 #else
 #include <CL/cl.h>
