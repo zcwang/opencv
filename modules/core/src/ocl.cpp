0a5f00c1e6bdec09fe2dc17cd80ce9a62f646007
Woo, Insoo <insoo.woo@intel.com>
2017-01-25 15:28:32 -0800

    Fix an issue with Kernel object reset release when consecutive Kernel::run calls
    Kernel::run launch OCL gpu kernels and set a event callback function
    to decreate the ref count of UMat or remove UMat when the lauched workloads
    are completed. However, for some OCL kernels requires multiple call of
    Kernel::run function with some kernel parameter changes (e.g., input
    and output buffer offset) to get the final computation result.
    In the case, the current implementation requires unnecessary
    synchronization and cleanupMat.
    
    This fix requires the user to specify whether there will be more work or not.
    If there is no remaining computation, the Kernel::run will reset the
    kernel object
    
    Signed-off-by: Woo, Insoo <insoo.woo@intel.com>

--- modules/core/src/ocl.cpp
+++ modules/core/src/ocl.cpp
@@ -3186,7 +3186,7 @@ struct Kernel::Impl
 
     void cleanupUMats()
     {
-        for( int i = 0; i < MAX_ARRS; i++ )
+        for( int i = 0; i < nu; i++ )
             if( u[i] )
             {
                 if( CV_XADD(&u[i]->urefcount, -1) == 1 )
@@ -3447,9 +3447,8 @@ int Kernel::set(int i, const KernelArg& arg)
     return i+1;
 }
 
-
 bool Kernel::run(int dims, size_t _globalsize[], size_t _localsize[],
-                 bool sync, const Queue& q)
+                 bool sync, const Queue& q, bool moreWorkDone)
 {
     CV_INSTRUMENT_REGION_OPENCL_RUN(p->name.c_str());
 
@@ -3470,11 +3469,11 @@ bool Kernel::run(int dims, size_t _globalsize[], size_t _localsize[],
     }
     if( total == 0 )
         return true;
-    if( p->haveTempDstUMats )
+    if( p->haveTempDstUMats && !moreWorkDone)
         sync = true;
     cl_int retval = clEnqueueNDRangeKernel(qq, p->handle, (cl_uint)dims,
                                            offset, globalsize, _localsize, 0, 0,
-                                           sync ? 0 : &p->e);
+                                           sync ? 0 : (moreWorkDone? 0: &p->e ));
 #if CV_OPENCL_SHOW_RUN_ERRORS
     if (retval != CL_SUCCESS)
     {
@@ -3485,9 +3484,9 @@ bool Kernel::run(int dims, size_t _globalsize[], size_t _localsize[],
     if( sync || retval != CL_SUCCESS )
     {
         CV_OclDbgAssert(clFinish(qq) == CL_SUCCESS);
-        p->cleanupUMats();
+        if (!moreWorkDone) p->cleanupUMats();
     }
-    else
+    else if (!moreWorkDone)
     {
         p->addref();
         CV_OclDbgAssert(clSetEventCallback(p->e, CL_COMPLETE, oclCleanupCallback, p) == CL_SUCCESS);
