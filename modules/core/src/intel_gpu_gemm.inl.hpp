1663e1e1bbac291d0b162d35a8b8531892ab2a50
Alexander Alekhin <alexander.alekhin@intel.com>
2017-03-02 15:55:38 +0300

    ocl: intel_gemm: change multiple kernels running scheme

--- modules/core/src/intel_gpu_gemm.inl.hpp
+++ modules/core/src/intel_gpu_gemm.inl.hpp
@@ -37,39 +37,39 @@ static bool intel_gpu_gemm(
     UMat A, Size sizeA,
     UMat B, Size sizeB,
     UMat D, Size sizeD,
-    double alpha, double beta, 
+    double alpha, double beta,
     bool atrans, bool btrans)
 {
-    sizeA; sizeB;
+    CV_UNUSED(sizeB);
 
-    int M = sizeD.height, N = sizeD.width, K = ((atrans)? sizeA.height : sizeA.width);
-
-    std::string kernelName;
     bool ret = true;
-    std::string opts;
 
-    int lx = 8, ly = 4;
-    int dx = 4, dy = 8;
+    const int M = sizeD.height, N = sizeD.width, K = ((atrans)? sizeA.height : sizeA.width);
+
+    const char* kernelName = NULL;
+
+    size_t lx = 8, ly = 4;
+    size_t dx = 4, dy = 8;
 
     if(!atrans && !btrans)
     {
         kernelName = "intelblas_gemm_buffer_NN";
         if (M % 32 == 0 && N % 32 == 0 && K % 16 == 0)
         {
-            kernelName += "_sp";
+            kernelName = "intelblas_gemm_buffer_NN_sp";
         }
     }
-    else if(atrans && !btrans) 
+    else if(atrans && !btrans)
     {
         kernelName = "intelblas_gemm_buffer_TN";
     }
-    else if(!atrans && btrans) 
+    else if(!atrans && btrans)
     {
         kernelName = "intelblas_gemm_buffer_NT";
         ly = 16;
         dx = 1;
-    } 
-    else 
+    }
+    else
     {
         kernelName = "intelblas_gemm_buffer_TT";
     }
@@ -78,51 +78,37 @@ static bool intel_gpu_gemm(
     const size_t gy = (size_t)(M + dy - 1) / dy;
 
     size_t local[] = {lx, ly, 1};
-    size_t global[] = {(gx + lx - 1) / lx * lx, (gy + ly - 1) / ly * ly, 1};
- 
-    ocl::Kernel k(kernelName.c_str(), cv::ocl::core::intel_gemm_oclsrc, opts);
-    if (k.empty())
-    {
-        return false;
-    }
+    size_t global[] = {gx, gy, 1};
 
-    int stride = (M * N < 1024 * 1024) ? 10000000 : 256;
-    k.args(ocl::KernelArg::PtrReadOnly(A),   // 0
-           (int) (A.offset / sizeof(float)),
-           ocl::KernelArg::PtrReadOnly(B),
-           (int) (B.offset / sizeof(float)),
-           ocl::KernelArg::PtrWriteOnly(D),
-           (int) (D.offset / sizeof(float)),
-           M, N, K,
-           (float)alpha,
-           (float)beta,
-           (int)(A.step / sizeof(float)),
-           (int)(B.step / sizeof(float)),
-           (int)(D.step / sizeof(float)),    // 13
-           (int) 0,                          // 14 start_index
-           stride);                          // 15
+    const int stride = ((!atrans && btrans) || (M * N < 1024 * 1024)) ? K : 256;
+
+    String errmsg;
+    const ocl::Program program = ocl::Context::getDefault().getProg(ocl::core::intel_gemm_oclsrc, "", errmsg);
 
     ocl::Queue q;
-    if(!atrans && btrans)
+    for(int start_index = 0; start_index < K; start_index += stride)
     {
-        ret = k.run(2, global, local, false, q, false);
+        ocl::Kernel k(kernelName, program);
+        if (k.empty())
+            return false;
+        k.args(ocl::KernelArg::PtrReadOnly(A),
+               (int) (A.offset / sizeof(float)),
+               ocl::KernelArg::PtrReadOnly(B),
+               (int) (B.offset / sizeof(float)),
+               ocl::KernelArg::PtrReadWrite(D),
+               (int) (D.offset / sizeof(float)),
+               M, N, K,
+               (float)alpha,
+               (float)beta,
+               (int)(A.step / sizeof(float)),
+               (int)(B.step / sizeof(float)),
+               (int)(D.step / sizeof(float)),
+               (int)start_index,
+               stride);
+        ret = k.run(2, global, local, false, q);
+        if (!ret) return ret;
     }
-    else
-    {
-        for(int start_index = 0; start_index < K; start_index += stride)
-        {
-       	    k.set(14, &start_index, sizeof(start_index));
-            if ((start_index + stride) < K)
-    	    {
-                ret = k.run(2, global, local, false, q, true);
-                if (!ret) return ret;
-    	    }
-    	    else
-            {
-                ret = k.run(2, global, local, false, q, false);
-    	    }
-        }
-    }
+    q.finish();
 
     return ret;
 }
@@ -130,4 +116,3 @@ static bool intel_gpu_gemm(
 } // namespace cv
 
 #endif
-
