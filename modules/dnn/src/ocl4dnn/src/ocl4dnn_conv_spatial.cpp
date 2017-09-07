702d27d9793c87fd0a5f4959f6d91f196e80464d
Alexander Alekhin <alexander.alekhin@intel.com>
2017-09-07 16:09:20 +0300

    ! test

--- modules/dnn/src/ocl4dnn/src/ocl4dnn_conv_spatial.cpp
+++ modules/dnn/src/ocl4dnn/src/ocl4dnn_conv_spatial.cpp
@@ -56,6 +56,8 @@
 #include <direct.h>
 #endif
 
+#define TEST_ALL_KERNELS
+
 #ifdef HAVE_OPENCL
 namespace cv { namespace dnn { namespace ocl4dnn {
 template<typename Dtype>
@@ -1514,8 +1516,8 @@ void OCL4DNNConvSpatial<Dtype>::prepareKernel(UMat &bottom, UMat &top,
         bestKernelConfig.release();
     }
 
-    if (loadCachedConfig())
-        return;
+    //if (loadCachedConfig())
+    //    return;
 
     tune(bottom, top, weight, bias, numImages);
 }
